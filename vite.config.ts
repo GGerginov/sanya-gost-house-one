9c44a810316f9d58cd46e1af679276eb192628b8
George Gerginov <ggerginov1291@gmail.com>
2025-08-18 16:44:47 +0300

    Update vite.config.ts: customize asset filenames in build outputs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]", // без хеш
+      }
+    }
+  },
   server: {
     host: "::",
     port: 8080,
